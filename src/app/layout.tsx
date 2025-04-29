af5174ef0dfbab7fd42141c6e920d4ec90d91705
ptkyaw505 <ptkyaw505@gmail.com>
2025-04-29 05:06:39 +0000

    fix: Remove vercel.json and update website url.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Media Extractor",
   description: "Extract and download various media types from websites with just a few clicks. Simple, fast, and reliable",
-  metadataBase: new URL('https://media-extractor-v2.onrender.com/'), // Good practice!
+  metadataBase: new URL('media-extractor.phyothihakyaw.com'),
   authors: [
     {
       name: 'Phyo Thiha Kyaw',
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     title: 'Media Extractor',
     description: 'Extract and download various media types from websites with just a few clicks. Simple, fast, and reliable',
     type: 'website',
-    url: 'https://media-extractor-v2.onrender.com/',
+    url: new URL('media-extractor.phyothihakyaw.com'),
     images: '/home.png',
     siteName: 'Media Extractor',
   },
